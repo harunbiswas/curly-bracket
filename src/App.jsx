3f4341fcdd46ad910b612a07d0ff42a9d539f0c2
Harun BIswas Rubel <harunbiswasrubel@gmail.com>
2024-06-25 17:03:08 +0600

    update 3 item

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,17 @@ import Curly from './components/Curly'
 
 function App() {
   return (
-    <div className='wrp'>
-      <Curly color='black' />
-    </div>
+    <>
+      <div className='wrp'>
+        <Curly color='black' />
+      </div>
+      <div className='wrp-1'>
+        <Curly color='black' />
+      </div>
+      <div className='wrp-2'>
+        <Curly color='black' />
+      </div>
+    </>
   )
 }
 
